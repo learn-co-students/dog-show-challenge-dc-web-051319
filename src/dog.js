c3f871101e2ec1d94381863aef82598572f74487
Matt Williams <matt.seongjun@gmail.com>
2019-07-02 15:00:45 -0400

    Functionally done. Don't like the quirks

---
+++ src/dog.js
@@ -0,0 +1,61 @@
+class Dog {
+    constructor(id, name, breed, sex) {
+        this.id = id;
+        this.name = name;
+        this.breed = breed;
+        this.sex = sex;
+        Dog.all.push(this);
+    }
+
+    static fetch(url) {
+        fetch(url)
+            .then(response => response.json())
+            .then(dogs => {
+                dogs.forEach(dog => {
+                    let dogObj = new Dog (
+                        dog.id,
+                        dog.name,
+                        dog.breed,
+                        dog.sex
+                    )
+                });
+                return Dog.all
+            })
+            .then(dogs => Adapter.render(dogs));
+    }
+
+    update(url, data) {
+        let updateData = {
+            id: this.id,
+            name: data.name,
+            breed: data.breed,
+            sex: data.sex
+        };
+        
+        let configObject = {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(updateData)
+        };
+        
+        fetch(`${url}/${this.id}`, configObject)
+            .then(response => response.json())
+            .then(returnedDog => {
+                this.name = returnedDog.name
+                this.breed = returnedDog.breed
+                this.sex = returnedDog.sex
+            })
+            .catch(error => {
+                window.alert(error.message);
+            });
+    }
+
+    static findByRowId(rowId) {
+        return Dog.all.find(dog => dog.id == rowId.split("-")[1])
+    }
+}
+
+Dog.all = [];
